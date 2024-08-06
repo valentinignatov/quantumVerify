d664e3f2ff38227780fbd7119460b8d6e74a4498
Valentin Ignatov <valentin.ignatov.97@gmail.com>
2024-08-06 22:26:52 +0300

    Add pom for addremovetest

--- src/test/java/tests/AddRemoveElementsTest.java
+++ src/test/java/tests/AddRemoveElementsTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import config.page.*;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -12,23 +13,20 @@ public class AddRemoveElementsTest extends BaseChromeConf {
 
 	@Test
 	public void testAddAndRemoveElements() throws InterruptedException {
-		driver.get("https://practice.expandtesting.com/add-remove-elements");
-
-		WebElement addButton = driver.findElement(By.xpath("//button[text()='Add Element']"));
-
-		for (int i = 0; i < 100; i++) {
-			addButton.click();
-		}
-
-		List<WebElement> deleteButtons = driver.findElements(By.className("added-manually"));
-		Assert.assertEquals(deleteButtons.size(), 100, "Number of added elements should be 100");
-
-		for (int i = 0; i < 100; i++) {
-			deleteButtons.get(i).click();
-		}
-
-		deleteButtons = driver.findElements(By.className("added-manually"));
-		Assert.assertEquals(deleteButtons.size(), 0, "All elements should be removed");
+		
+		AddRemovePage addRemPage = new AddRemovePage(driver);
+		
+		addRemPage.get();
+		
+		int elements = 100;
+		
+		addRemPage.addElement(elements);
+		
+		Assert.assertEquals(addRemPage.checkAddedElements(), 100, "Number of added elements should be 100");
+		
+		addRemPage.removeElements(elements);
+		
+		Assert.assertEquals(addRemPage.checkAddedElements(), 0, "\"All elements should be removed");
 	}
 
 }
